92fec16003865730c5ebb98c6c22f4257afc4f13
rmit-s3477133-Samuel-Russo <s3477133@student.rmit.edu.au>
2016-08-21 18:33:43 +1000

    Added Config File with small amount of details

---
+++ src/elasticsearch_analyzer/Config.java
@@ -0,0 +1,10 @@
+package elasticsearch_analyzer;
+
+public class Config {
+
+	
+	public static final String elasticsearch_Repository = "https://github.com/elastic/elasticsearch";
+	
+	public static final boolean only_Commits_with_merge = true;
+	
+}
