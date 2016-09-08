df38da7e8576de27f90b2ff549956d4362f4aca4
Samuel Russo <s3477133@student.rmit.edu.au>
2016-09-08 19:45:02 +1000

    Added check in config if all issues to be included or just ones with git commits

--- src/elasticsearch_analyzer/Config.java
+++ src/elasticsearch_analyzer/Config.java
@@ -4,5 +4,6 @@ public class Config {
 
 	public static final String git_OAuth = "4632ed7598221d6a99b34c3eaa5e851fd9fae79d";
 	public static final String git_Repository = "elastic/elasticsearch";
+	public static final boolean git_AllIssues = true;
 	
 }
