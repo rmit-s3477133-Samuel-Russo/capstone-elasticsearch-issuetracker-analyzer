17e1649e7ee66c765fe362b6eb9c6ec17f403b7d
Samuel Russo <s3477133@student.rmit.edu.au>
2016-09-10 16:43:58 +1000

    Added CSV output code

---
+++ src/elasticsearch_analyzer/CSV.java
@@ -0,0 +1,44 @@
+package elasticsearch_analyzer;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import com.csvreader.CsvWriter;
+
+public class CSV {
+
+	public static CSV csv;
+	
+	public CSV getInstance() throws IOException{
+		if (csv == null){
+			csv = new CSV();
+		}
+		return csv;
+	}
+	
+	private CsvWriter csvOutput;
+	
+	private CSV() throws IOException{
+		boolean csvExists = new File(Config.csv_File).exists();
+		
+		csvOutput = new CsvWriter(new FileWriter(Config.csv_File, true), ',');
+		
+		if (!csvExists){
+			write("Issue Number", "Issue Title");
+		}
+		
+	}
+	
+	public void write(String number, String title) throws IOException{
+		csvOutput.write(number);
+		csvOutput.write(title);
+		csvOutput.endRecord();
+	}
+	
+	public void close(){
+		csvOutput.close();
+		csv = null;
+	}
+	
+}
