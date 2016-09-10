badc43aafb6b63cd2ff834dbaef8a39fe9025f41
Samuel Russo <s3477133@student.rmit.edu.au>
2016-09-10 17:07:40 +1000

    Added First portion of Git Connection and retrival of issue number and title

--- src/elasticsearch_analyzer/Config.java
+++ src/elasticsearch_analyzer/Config.java
@@ -1,5 +1,10 @@
 package elasticsearch_analyzer;
 
+import java.util.EnumMap;
+
+import com.jcabi.github.Issues;
+import com.jcabi.github.Issues.Qualifier;
+
 public class Config {
 
 	public static final String git_OAuth = "4632ed7598221d6a99b34c3eaa5e851fd9fae79d";
@@ -7,5 +12,15 @@ public class Config {
 	public static final boolean git_AllIssues = true;
 	
 	public static final String csv_File = "output.csv";
+	
+	public static EnumMap<Qualifier, String> getQualifier(){
+		
+		EnumMap<Issues.Qualifier,String> qualifiers = new EnumMap<Issues.Qualifier,String>(Issues.Qualifier.class);
+		qualifiers.put(Issues.Qualifier.LABELS, "bug");
+		qualifiers.put(Issues.Qualifier.STATE, "closed");
+		
+		return qualifiers;
+		
+	}
 	
 }
