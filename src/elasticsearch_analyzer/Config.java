1fb68acf3eb3b1e26f7b391fce489f7aa7a2ba4a
rmit-s3487468-brendon-jansz <s3487468@student.rmit.edu.au>
2016-09-12 13:09:12 +1000

    Comments
    Description for each section

--- src/elasticsearch_analyzer/Config.java
+++ src/elasticsearch_analyzer/Config.java
@@ -7,10 +7,15 @@ import com.jcabi.github.Issues.Qualifier;
 
 public class Config {
 
+	// OAuth token 
 	public static final String git_OAuth = "4632ed7598221d6a99b34c3eaa5e851fd9fae79d";
+	
+	// Repository to be used
 	public static final String git_Repository = "elastic/elasticsearch";
+	
 	public static final boolean git_AllIssues = true;
 	
+	// CSV file output name
 	public static final String csv_File = "output.csv";
 	
 	public static EnumMap<Qualifier, String> getQualifier(){
--- src/elasticsearch_analyzer/Main.java
+++ src/elasticsearch_analyzer/Main.java
@@ -9,19 +9,27 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
+		
+		// Creating a Github object using OAuth token from config class
 		Github github = new RtGithub(Config.git_OAuth);
+		
+		// Creating a Repo object that uses the elasic search repository from config class
 		Repo repository = github.repos().get(new Coordinates.Simple(Config.git_Repository));
 		
+		// Creating an Issues object from the repository
 		Issues issues = repository.issues();
 		
+		// Creating an object that can search issues in descending order and use qualifiers in getQualifier method from config class
 		Iterable<Issue> iIssues = issues.search(Issues.Sort.UPDATED, Search.Order.DESC, Config.getQualifier());
 		
+		// Iterating through the issues for closed bugs in descending order
 		for(Iterator<Issue> issueIndex = iIssues.iterator(); issueIndex.hasNext(); ) {
 			
 			Issue issumeItem = issueIndex.next();
 
 		    Issue.Smart issueItemFull = new Issue.Smart(issumeItem);
-
+            
+		    // Prints the issue name and number on each line
 		    System.out.println(issueItemFull.title() + " - " + issueItemFull.number());
 		    
 		}
