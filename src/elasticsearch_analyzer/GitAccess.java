74d004204e58037209e9a55d01e20cbc763702da
Samuel Russo <s3477133@student.rmit.edu.au>
2016-09-14 13:43:40 +1000

    Restructure of GitHub Access Class

---
+++ src/elasticsearch_analyzer/GitAccess.java
@@ -0,0 +1,51 @@
+package elasticsearch_analyzer;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+import com.jcabi.github.Coordinates;
+import com.jcabi.github.Github;
+import com.jcabi.github.Issue;
+import com.jcabi.github.Issues;
+import com.jcabi.github.Repo;
+import com.jcabi.github.RtGithub;
+import com.jcabi.github.Search;
+
+public class GitAccess {
+
+	private static GitAccess GitAccess;
+	
+	public static GitAccess getInstance(){
+		
+		if (GitAccess == null){
+			GitAccess = new GitAccess();
+		}
+		
+		return GitAccess;
+	}
+	
+	private Github github;
+	private Repo repository;
+	private Issues issues;
+	
+	private GitAccess(){
+		github = new RtGithub(Config.git_OAuth);
+		repository = github.repos().get(new Coordinates.Simple(Config.git_Repository));
+		issues = repository.issues();
+	}
+	
+	public void Search() throws IOException{
+		Iterable<Issue> iIssues = issues.search(Issues.Sort.UPDATED, Search.Order.DESC, Config.getQualifier());
+		
+		for(Iterator<Issue> issueIndex = iIssues.iterator(); issueIndex.hasNext(); ) {
+			
+			Issue issumeItem = issueIndex.next();
+
+		    Issue.Smart issueItemFull = new Issue.Smart(issumeItem);
+
+		    System.out.println(issueItemFull.title() + " - " + issueItemFull.number());
+		    
+		}
+	}
+	
+}
--- src/elasticsearch_analyzer/Main.java
+++ src/elasticsearch_analyzer/Main.java
@@ -1,30 +1,13 @@
 package elasticsearch_analyzer;
 
 import java.io.IOException;
-import java.util.Iterator;
-
-import com.jcabi.github.*;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		Github github = new RtGithub(Config.git_OAuth);
-		Repo repository = github.repos().get(new Coordinates.Simple(Config.git_Repository));
 		
-		Issues issues = repository.issues();
-		
-		Iterable<Issue> iIssues = issues.search(Issues.Sort.UPDATED, Search.Order.DESC, Config.getQualifier());
-		
-		for(Iterator<Issue> issueIndex = iIssues.iterator(); issueIndex.hasNext(); ) {
-			
-			Issue issumeItem = issueIndex.next();
-
-		    Issue.Smart issueItemFull = new Issue.Smart(issumeItem);
-
-		    System.out.println(issueItemFull.title() + " - " + issueItemFull.number());
-		    
-		}
+		GitAccess.getInstance().Search();
 		
 	}
 
