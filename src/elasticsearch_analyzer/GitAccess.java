62ea8b44b3d8c6368c563007992c3cf08599326d
Samuel Russo <s3477133@student.rmit.edu.au>
2016-09-21 09:23:26 +1000

    Added Work in Seperate Stages as searching for the pull requests from the api causes the JCABI to through illegalstateexception, I alot of work around had to be tempory implemented to obtain all data

--- src/elasticsearch_analyzer/GitAccess.java
+++ src/elasticsearch_analyzer/GitAccess.java
@@ -1,8 +1,10 @@
 package elasticsearch_analyzer;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Iterator;
 
+import com.csvreader.CsvReader;
 import com.jcabi.github.*;
 
 public class GitAccess {
@@ -21,11 +23,13 @@ public class GitAccess {
 	private Github github;
 	private Repo repository;
 	private Issues issues;
+	private Pulls pull;
 	
 	private GitAccess(){
 		github = new RtGithub(Config.git_OAuth);
 		repository = github.repos().get(new Coordinates.Simple(Config.git_Repository));
 		issues = repository.issues();
+		pull = repository.pulls();
 	}
 	
 	public void Search(){
@@ -62,6 +66,83 @@ public class GitAccess {
 		    
 		}
 		
+	}
+	
+	public void SearchIsMerged(){
+		try {
+			CsvReader cv = new CsvReader("mergecommitsha.csv");
+			int recordnumber = 0;
+			int writtenrecord = 0;
+			cv.readHeaders();
+			
+			while(cv.readRecord()){
+				String inum = cv.get("Issue Number");
+				String ititle = cv.get("Issue Title");
+				String isha = cv.get("Commit ID");
+				
+				Pull.Smart pullrequest = new Pull.Smart(pull.get(Integer.parseInt(inum)));
+				System.out.println("Processing Record - " + ++recordnumber);
+				
+				boolean isMergedTrue = pullrequest.json().getBoolean("merged");
+				if (isMergedTrue){
+					CSV.getInstance().write(inum, ititle, isha);
+		    		CSV.getInstance().close();
+		    		System.out.println("Written Record - " + ++writtenrecord);
+				}
+				
+			}
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void SearchIsMergedRepairSha(){
+		try {
+			CsvReader cv = new CsvReader("mergecommitshaismerged.csv");
+			int recordnumber = 0;
+			cv.readHeaders();
+			
+			while(cv.readRecord()){
+				String inum = cv.get("Issue Number");
+				String ititle = cv.get("Issue Title");
+				String isha = cv.get("Commit ID");
+				
+		
+				System.out.println("Processing Record - " + ++recordnumber + " - " + inum);
+				
+
+				Pull.Smart pullrequest = new Pull.Smart(pull.get(Integer.parseInt(inum)));
+				Issue.Smart issue = new Issue.Smart(pullrequest.issue());
+				Iterable<Event> Events = issue.events();
+				for(Iterator<Event> eventIndex = Events.iterator(); eventIndex.hasNext(); ){
+					Event.Smart event = new Event.Smart(eventIndex.next());
+					String checkMerged = event.json().getString("event");
+					if (checkMerged.matches("merged")){
+						String commit_id = event.json().getString("commit_id");
+						isha = commit_id;
+						CSV.getInstance().write(inum, ititle, isha);
+			    		CSV.getInstance().close();
+			    		System.out.println("Written New Sha - " + isha);
+						break;
+					}
+				}
+				
+				
+				
+			}
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 }
--- src/elasticsearch_analyzer/Main.java
+++ src/elasticsearch_analyzer/Main.java
@@ -5,8 +5,12 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		GitAccess.getInstance().Search();
-		
+		// Stage 1 - Obtain Pull Requests that are closed and with a label of bug
+		GitAccess.getInstance().Search(); 
+		// Stage 2 - Obtain only Merged Pull Requests from data obtained in Stage 1
+		GitAccess.getInstance().SearchIsMerged(); 
+		// Stage 3 - Required merge commit sha from event chain as older pull requests doesn't hold the correct merge commit sha that is visible from the API 
+		GitAccess.getInstance().SearchIsMergedRepairSha(); 
 	}
 
 }
