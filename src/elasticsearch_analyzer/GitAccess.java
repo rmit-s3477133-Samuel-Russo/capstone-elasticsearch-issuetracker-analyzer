089415c511d3a58b59cb328a38f43751f0776846
Samuel Russo <s3477133@student.rmit.edu.au>
2016-09-22 16:44:04 +1000

    Added Stage 5 - Summarys & Stage 6 - Combine Summary and Diff

--- src/elasticsearch_analyzer/GitAccess.java
+++ src/elasticsearch_analyzer/GitAccess.java
@@ -3,6 +3,7 @@ package elasticsearch_analyzer;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -12,6 +13,9 @@ import java.io.Writer;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.Iterator;
+
+import javax.json.JsonArray;
+import javax.json.JsonObject;
 
 import com.csvreader.CsvReader;
 import com.jcabi.github.*;
@@ -33,12 +37,14 @@ public class GitAccess {
 	private Repo repository;
 	private Issues issues;
 	private Pulls pull;
+	private RepoCommits commits;
 	
 	private GitAccess(){
 		github = new RtGithub(Config.git_OAuth);
 		repository = github.repos().get(new Coordinates.Simple(Config.git_Repository));
 		issues = repository.issues();
 		pull = repository.pulls();
+		commits = repository.commits();
 	}
 	
 	public void Search(){
@@ -165,7 +171,7 @@ public class GitAccess {
 				String inum = cv.get("Issue Number");
 				
 				URL website = new URL("https://github.com/elastic/elasticsearch/pull/" + inum + ".diff");
-		        URLConnection connection = website.openConnection();
+		        URLConnection connection = website.openConnection();		        
 		        BufferedReader in = new BufferedReader(
 		                                new InputStreamReader(
 		                                    connection.getInputStream()));
@@ -191,6 +197,117 @@ public class GitAccess {
 			}
 
 			
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void ObtainSummaries(){
+		try {
+			CsvReader cv = new CsvReader("mergecommitshaismergedfixed.csv");
+			int recordnumber = 0;
+			cv.readHeaders();
+			
+			while(cv.readRecord()){
+				String inum = cv.get("Issue Number");
+				String isha = cv.get("Commit ID");
+
+				System.out.println("Processing Record - " + ++recordnumber + " - " + inum);		
+				
+				
+				RepoCommit.Smart commit = new RepoCommit.Smart(commits.get(isha));
+				
+				StringBuilder commitSummary = new StringBuilder();
+				
+				commitSummary.append("Commit: " + isha + "\r\n");
+				
+				JsonObject commitJson = commit.json();
+				
+				JsonArray commitParentArray = commitJson.getJsonArray("parents");
+				
+				for(Object parentIndex : commitParentArray){
+					JsonObject parent = (JsonObject)parentIndex;
+					commitSummary.append("Parent: " + parent.getString("sha") + "\r\n");
+				}
+				
+				
+				JsonObject commitAuthor = commitJson.getJsonObject("commit").getJsonObject("author");
+				commitSummary.append("Author: " + commitAuthor.getString("name") + " <" + commitAuthor.getString("email") + "> \r\n\r\n");
+				
+				commitSummary.append(commit.message() + "\r\n\r\n");
+				
+				File fileDir = new File(inum + ".txt");
+				
+				Writer out = new BufferedWriter(new OutputStreamWriter(
+					new FileOutputStream(fileDir), "UTF8"));
+				
+				out.append(commitSummary);
+				out.close();
+				
+			}
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void CombineSummaryDiff(){
+		try {
+			CsvReader cv = new CsvReader("mergecommitshaismergedfixed.csv");
+			int recordnumber = 0;
+			cv.readHeaders();
+			
+			while(cv.readRecord()){
+				String inum = cv.get("Issue Number");
+				String ititle = cv.get("Issue Title");
+						
+				System.out.println("Processing Record - " + ++recordnumber + " - " + inum);
+				
+				StringBuilder output = new StringBuilder();
+				
+				File fileSum = new File("sum/" + inum + ".txt");
+				File fileDiff = new File("diff/" + inum + ".txt");
+				File fileDir = new File(inum + ".txt");
+				
+				output.append("Issue Number: " + inum + "\r\n");
+				output.append("Issue Title: " + ititle + "\r\n\r\n");
+				
+				BufferedReader in1 = new BufferedReader(
+                        new InputStreamReader(new FileInputStream(fileSum)));
+				String inputLine1;
+				while ((inputLine1 = in1.readLine()) != null) 
+					output.append(inputLine1 + "\r\n");
+
+		        in1.close();
+		        
+		        output.append("\r\n");
+				
+		        String inputLine2;
+		        BufferedReader in2 = new BufferedReader(
+                        new InputStreamReader(new FileInputStream(fileDiff)));
+				
+				while ((inputLine2 = in2.readLine()) != null) 
+					output.append(inputLine2 + "\r\n");
+
+		        in2.close();
+		        
+				
+				Writer out = new BufferedWriter(new OutputStreamWriter(
+					new FileOutputStream(fileDir), "UTF8"));
+				
+				out.append(output);
+				out.close();
+				
+			}
 			
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
--- src/elasticsearch_analyzer/Main.java
+++ src/elasticsearch_analyzer/Main.java
@@ -5,6 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		// Only 1 Stage at a time to be ran, comment out stages not in use
+		
 		// Stage 1 - Obtain Pull Requests that are closed and with a label of bug
 		GitAccess.getInstance().Search(); 
 		// Stage 2 - Obtain only Merged Pull Requests from data obtained in Stage 1
@@ -13,6 +15,10 @@ public class Main {
 		GitAccess.getInstance().SearchIsMergedRepairSha(); 
 		// Stage 4 - Obtain Git Diff from Remote Github Server - very efficient in comparison to Jgit & Bash/Powershell
 		GitAccess.getInstance().PullDiff();
+		// Stage 5 - Obtain Git Commit Summary
+		GitAccess.getInstance().ObtainSummaries();
+		// Stage 6 - Combine Summary and Diff
+		GitAccess.getInstance().CombineSummaryDiff();
 	}
 
 }
